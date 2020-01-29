07ca5c1b9b39874228193e0cf2a1c61001a6d6a8
apple <apple@apples-MacBook-Pro-2.local>
2020-01-28 19:34:02 -0800

    on step 2

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, useRouteMatch } from 'react-router-dom';
+import Movie from './Movie';
+
+
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -29,7 +33,13 @@ const MovieList = props => {
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
+  // console.log('movielist' , movie);
+  const match = useRouteMatch()
+  console.log(match.path);
+
   return (
+<Link to='/Movies/:id' >
+  
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -46,6 +56,7 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+</Link>
   );
 }
 
